8b2aebc2fd08e74c87ef280858dcc057c79905a3
Ramiro Meneses Prattes <xanco78@gmail.com>
2018-11-13 15:40:34 -0300

    Udpating the project to github

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -9,9 +9,12 @@ export class TicTacToe {
     }
     handleClick(squareElement) {
         const id = squareElement.id.split(" ")
-        this.board[id[0]][id[1]] = this.turn
-        squareElement.textContent = this.turn
-        this.changeTurns()
+        if (!this.board[id[0]][id[1]]) {
+            this.board[id[0]][id[1]] = this.turn
+            squareElement.textContent = this.turn
+            this.changeTurns()
+            console.log(this.board)
+        }
     }
     changeTurns() {
         if (this.turn === "x") {
@@ -19,5 +22,23 @@ export class TicTacToe {
         } else if (this.turn === "o") {
             this.turn = "x"
         }
+    }
+    checkWin() {
+        const winPatterns = {
+            //horizontales
+            pattern1: [this.board[0][0], this.board[0][1], this.board[0][2]],
+            pattern2: [this.board[1][0], this.board[1][1], this.board[1][2]],
+            pattern3: [this.board[2][0], this.board[2][1], this.board[2][2]],
+            //verticales
+            pattern4: [this.board[0][0], this.board[1][0], this.board[2][0]],
+            pattern5: [this.board[0][1], this.board[1][1], this.board[2][1]],
+            pattern6: [this.board[0][2], this.board[1][2], this.board[2][2]],
+            //diagonales
+            pattern7: [this.board[0][0], this.board[1][1], this.board[2][2]],
+            pattern8: [this.board[0][2], this.board[1][1], this.board[2][0]],
+
+        }
+
+
     }
 }
