bed162f9dc8b14ff7fc75e2703d428175a785030
Ramiro Meneses <xanco78@gmail.com>
2018-11-13 21:04:29 -0300

    Messy code for checking win, but works. Changes the TicTacToe.status property to the player who wins

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -6,6 +6,7 @@ export class TicTacToe {
             ["", "", ""]
         ]
         this.turn = turn
+        this.state = "playing"
     }
     handleClick(squareElement) {
         const id = squareElement.id.split(" ")
@@ -13,7 +14,8 @@ export class TicTacToe {
             this.board[id[0]][id[1]] = this.turn
             squareElement.textContent = this.turn
             this.changeTurns()
-            console.log(this.board)
+            //console.table(this.board)
+            console.log(this.checkWin())
         }
     }
     changeTurns() {
@@ -24,6 +26,8 @@ export class TicTacToe {
         }
     }
     checkWin() {
+        let xWin = false
+        let oWin = false
         const winPatterns = {
             //horizontales
             pattern1: [this.board[0][0], this.board[0][1], this.board[0][2]],
@@ -38,6 +42,101 @@ export class TicTacToe {
             pattern8: [this.board[0][2], this.board[1][1], this.board[2][0]],
 
         }
+
+    //Check patterns
+    // This code can be optimized a lot. Just wanted to make a solution for now. Will be omptimized :)
+       xWin = winPatterns.pattern1.every(element => element === "x")
+       oWin = winPatterns.pattern1.every(element => element === "o")
+       
+       if(xWin){
+           this.state = "x-win"
+           return this.state
+       }else if(oWin){
+            this.state = "o-win"
+           return this.state
+       }
+
+       xWin = winPatterns.pattern2.every(element => element === "x")
+       oWin = winPatterns.pattern2.every(element => element === "o")
+
+       if(xWin){
+        this.state = "x-win"
+        return this.state
+    }else if(oWin){
+         this.state = "o-win"
+        return this.state
+    }
+
+       xWin = winPatterns.pattern3.every(element => element === "x")
+       oWin = winPatterns.pattern3.every(element => element === "o")
+
+       if(xWin){
+           this.state = "x-win"
+           return this.state
+       }else if(oWin){
+            this.state = "o-win"
+           return this.state
+       }
+
+       xWin = winPatterns.pattern4.every(element => element === "x")
+       oWin = winPatterns.pattern4.every(element => element === "o")
+
+       if(xWin){
+           this.state = "x-win"
+           return this.state
+       }else if(oWin){
+            this.state = "o-win"
+           return this.state
+       }
+       
+
+       xWin = winPatterns.pattern5.every(element => element === "x")
+       oWin = winPatterns.pattern5.every(element => element === "o")
+
+       if(xWin){
+        this.state = "x-win"
+        return this.state
+    }else if(oWin){
+         this.state = "o-win"
+        return this.state
+    }
+    
+
+       xWin = winPatterns.pattern6.every(element => element === "x")
+       oWin = winPatterns.pattern6.every(element => element === "o")
+
+       if(xWin){
+        this.state = "x-win"
+        return this.state
+    }else if(oWin){
+         this.state = "o-win"
+        return this.state
+    }
+    
+
+       xWin = winPatterns.pattern7.every(element => element === "x")
+       oWin = winPatterns.pattern7.every(element => element === "o")
+
+       if(xWin){
+        this.state = "x-win"
+        return this.state
+    }else if(oWin){
+         this.state = "o-win"
+        return this.state
+    }
+    
+
+       xWin = winPatterns.pattern8.every(element => element === "x")
+       oWin = winPatterns.pattern8.every(element => element === "o")
+
+       if(xWin){
+        this.state = "x-win"
+        return this.state
+    }else if(oWin){
+         this.state = "o-win"
+        return this.state
+    }
+    
 
 
     }
